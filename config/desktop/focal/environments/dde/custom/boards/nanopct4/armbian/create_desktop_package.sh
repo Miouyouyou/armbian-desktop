ab39d5062ee57660ce12567706c3049963c71511
richneese <r.neese@gmail.com>
2020-10-20 17:05:57 -0400

    moved from old version up to armbian build head
    did all the patching and cleaning to brin it to head also fixed output from main.sh to show the new output  ....

--- config/desktop/focal/environments/dde/custom/boards/nanopct4/armbian/create_desktop_package.sh
+++ config/desktop/focal/environments/dde/custom/boards/nanopct4/armbian/create_desktop_package.sh
@@ -1,7 +1,9 @@
 # install custom xorg for pinebook-pro
+mkdir -p "${destination}"/etc/X11/
 cp -R "${SRC}"/packages/bsp/rk3399/xorg.conf "${destination}"/etc/X11/
 
 # install custom asound state for pinebook-pro
-cp -R "${SRC}"/packages/asound.state/ "${destination}"/etc/
+mkdir -p "${destination}"/etc/
+cp -R "${SRC}"/packages/blobs/asound.state/ "${destination}"/etc/
 
 	
